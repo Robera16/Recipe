4b2eef5894929cd77e51c1d92c6e17506110af43
robera16 <roberaworknehrobwork@gmail.com>
2022-03-01 23:28:03 +0300

    search result page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,7 +1,29 @@
+import { useLocation } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+
+// styles
 import './Search.css'
 
+// components
+import RecipeList from '../../components/RecipeList'
+
 export default function Search() {
+  // get query string ?q=veggie
+  const queryString = useLocation().search
+  // change to javascript object
+  const queryParams = new URLSearchParams(queryString)
+  // the value of query parameter 'q'
+  const query = queryParams.get('q')
+
+  const url = ' http://localhost:3000/recipes?q=' + query
+  const {error, isPending, data}  = useFetch(url)
+
   return (
-    <div>Search</div>
+    <div>
+      <h2 className='page-title'>Recipes including "{query}"</h2>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && <RecipeList recipes={data}/>}
+    </div>
   )
 }
