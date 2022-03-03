fdc7ea8e1764af2d67bc991b25f67a810e760076
robera16 <roberaworknehrobwork@gmail.com>
2022-03-03 16:41:12 +0300

    added real time data update

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -15,8 +15,7 @@ export default function Home() {
   
   useEffect(() => {
     setIsPending(true)
-    projectFirestore.collection('recipes').get()
-    .then((snapshot) => {
+    const unsub = projectFirestore.collection('recipes').onSnapshot((snapshot) => {
       if(snapshot.empty){
         setError('No recipes to load')
         setIsPending(false)
@@ -29,10 +28,12 @@ export default function Home() {
         setData(results)
         setIsPending(false)
       }
-    }).catch(err => {
+    }, (err) => {
       setError(err.message)
       setIsPending(false)
-    }) 
+    })
+    // fires when we navigate to other page
+    return () => unsub()
   }, [])
 
   return (
