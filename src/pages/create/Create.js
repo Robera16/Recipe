12edc04567797156817b6d8cdcbee5530df847d0
robera16 <roberaworknehrobwork@gmail.com>
2022-03-03 12:44:36 +0300

    adding & deleting to firestore

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect} from 'react'
-import { useFetch } from '../../hooks/useFetch'
 import { useHistory } from "react-router-dom"
+import {projectFirestore} from '../../firebase/config';
 // styles
 import './Create.css'
 
@@ -13,13 +13,16 @@ export default function Create() {
   const ingredientInput = useRef(null)
   const history = useHistory()
 
-  const {postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST')
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    // console.log(title, method, cookingTime, ingredients)
-    postData({ title, ingredients, method, cookingTime: cookingTime + ' minutes'})
-
+    const doc = { title, ingredients, method, cookingTime: cookingTime + ' minutes'}
+    try{
+      await projectFirestore.collection('recipes').add(doc)
+      history.push('/')
+    }catch(err){
+      console.log(err)
+    }
   }
 
   const handleAdd = (e) => {
@@ -32,13 +35,6 @@ export default function Create() {
     setNewIngredient('')
     ingredientInput.current.focus()
   }
-
-  useEffect(() => {
-    if(data){
-      // redirect
-      history.push('/')
-    }
-  }, [data])
 
   return (
     <div className='create'>
